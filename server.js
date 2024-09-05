1062e7be38e99555392cdb23b2479cd9901008fd
sreyesg <santiagoreyes.g@gmail.com>
2024-09-04 22:35:24 -0700

    add home and weather route in server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express =require('express')
 const app = express()
+// const bodyParser = require('body-parser')
 
-
+app.use(express.urlencoded( {extended: true}))
+app.use(express.json())
 
 app.get('/',(req, res)=> {
     res.render('index.ejs')
 })
 
-
+app.post('/weather',(req, res)=> {
+    console.log(req.body)
+})
 
 app.listen(4000)
