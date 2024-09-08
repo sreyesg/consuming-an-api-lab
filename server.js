3340b0538b57af7b0f3c51f3af1c784414ffcfba
sreyesg <santiagoreyes.g@gmail.com>
2024-09-08 15:20:06 -0700

    handle post router for /wheater

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express =require('express')
 const app = express()
 // const bodyParser = require('body-parser')
 
+// Middleware
 app.use(express.urlencoded( {extended: true}))
 app.use(express.json())
 
+// home route
 app.get('/',(req, res)=> {
     res.render('index.ejs')
 })
-
+// post route for Wheater
 app.post('/weather',(req, res)=> {
     console.log(req.body)
 })
